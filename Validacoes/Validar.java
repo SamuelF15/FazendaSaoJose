1c9878657028d18295edfcdc2e90cbdf588670aa
Pedro Torrezani <pedro.gustavo6565@gmail.com>
2023-11-08 22:10:51 -0300

    Validacoes feitas

--- Validacoes/Validar.java
+++ Validacoes/Validar.java
@@ -4,23 +4,31 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Validar {
-	// validando Nome da pessoa
-	public static boolean validarNomePessoa(String NomePess) {
+	// validadores bases
+	public static boolean validarNomesLetras(String NomePess) {
 		String ValidarNomP = "^[A-Za-z]{85}";
 		Pattern compiladorNom = Pattern.compile(ValidarNomP);
 		Matcher verificadorNom = compiladorNom.matcher(NomePess);
 		if (verificadorNom.matches()) {
-			System.out.println("Nome válido!!");
+			System.out.println("Válidado");
 			return true;
 		} else {
-			System.out.println("Nome inválido!!");
+			System.out.println("Inválidado");
 			return false;
 		}
 	}
-
-	// validando CPF
-	// public static boolean validarCPF(String CPF){}
-	// validando Data
+	public static boolean validarCaracteres(String senha){
+		String ValidarNomP = "^[A-Za-z0-9._-@]{8,20}";
+		Pattern compiladorNom = Pattern.compile(ValidarNomP);
+		Matcher verificadorNom = compiladorNom.matcher(NomePess);
+		if (verificadorNom.matches()) {
+			System.out.println("Válidado");
+			return true;
+		} else {
+			System.out.println("Inválidado");
+			return false;
+		}
+	}
 	public static boolean validarData(String Data) {
 		String ValidarData = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(19\\d{2}|20\\d{2})$";
 
@@ -35,8 +43,6 @@ public class Validar {
 		}
 
 	}
-
-	// validando Telefone
 	public static boolean validarTelefone(String Telefone) {
 		String ValidarTel = "^[0-9]{11}";
 		Pattern compiladorTel = Pattern.compile(ValidarTel);
@@ -50,8 +56,19 @@ public class Validar {
 			return false;
 		}
 	}
+	public static boolean validarCEP(String CEP) {
+		String ValidarTel = "^[0-9]{8}";
+		Pattern compiladorTel = Pattern.compile(ValidarTel);
+		Matcher verificadorTel = compiladorTel.matcher(CEP);
 
-	// validando Email
+		if (verificadorTel.matches()) {
+			System.out.println("CEP válido!!");
+			return true;
+		} else {
+			System.out.println("CEP inválido!!");
+			return false;
+		}
+	}
 	public static boolean validarEmail(String Email) {
 		String ValidarEmail = "^[A-Za-z0-9._-]+@(gmail\\.com|outlook\\.com|hotmail\\.com|live\\.com|uol\\.com\\.br|bol\\.com\\.br|yahoo\\.com|ymail\\.com|globomail\\.com)$";
 
@@ -66,13 +83,58 @@ public class Validar {
 		}
 	}
 
+    //Relacionados a Endereco
 	public static boolean ValidarCEPClient(String CEP){
-		ValidarCEPClient client = new  
-
-
-
-
-	}public stat
+		if(validarCEP(getCEP())){
+			return true
+		}else{
+			return false
+		}
+		}
+	}
+	public static boolean validarCidade(getCidade()){
+		if (validarNomesLetras(getCidade())){
+			return true
+		}else{
+			return false
+		}
+	}
+	public static boolean validarRua(getRua()){
+		if (validarNomesLetras(getRua())){
+			return true
+		}else{
+			return false
+		}
+	}
+	public static boolean validarBairro(getBairro()){
+		if (validarNomesLetras(getBairro())){
+			return true
+		}else{
+			return false
+		}
+	}
+	public static boolean validarComplemento(getComplemento()){
+		if (validarNomesLetras(getComplemento())){
+			return true
+		}else{
+			return false
+		}
+	}
+	//Relacionados a usuarios 
+	public static boolean validarNomeDeUsuario(){
+		if (validarNomesLetras(getNomeUser())){
+			return true
+		}else{
+			return false
+		}
+	}
+	public static boolean validarSenha(){
+		if (validarCaracteres(getNomeUser())){
+			return true
+		}else{
+			return false
+		}
+	}
 
 }
 
